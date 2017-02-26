329e20f4c6c055b07859d47534f88d9995714d45
Shomurod Yigitaliev <yigitaliev@lsbln.tengelmann.de>
2017-02-26 21:06:06 +0100

    Second

--- Calculate.java
+++ Calculate.java
@@ -2,7 +2,12 @@ public class Calculate
 {
     public static void main(String[] args)
     {
-	System.out.println("Calculater");
+	System.out.println("Calculate...");
+		int first = Integer.valueOf(args[0]);
+		int second = Integer.valueOf(args[1]);
+		int sum = first + second;
+		System.out.println(" Result: " + sum);
+	
 
 
 	}
